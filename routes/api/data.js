13efa0e5458cb0f95b2841a3d16d818226fec626
Radoslav Radev <rrradev@users.noreply.github.com>
2020-06-06 14:52:13 +0300

    Adding POST and GET on /api/data

---
+++ routes/api/data.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const router = express.Router();
+
+//Sensor data model
+const Data = require('../../models/Data');
+
+// @route   GET /api/data
+// @desc    Get all sensor data 
+// @acc     Public (for now)
+router.get('/', (req, res) => {
+    console.log("GET req");
+    Data.find()
+    .sort({date: -1})
+    .then(data => res.json(data));
+});
+
+// @route   POST /api/data
+// @desc    Post sensor data to db
+// @acc     Public (for now)
+router.post('/', (req, res) => {
+    console.log('POST req');
+    const newData = new Data({
+        pm25: req.body.pm25,
+        pm10: req.body.pm10
+    });
+     newData.save()
+     .then(data => res.json(data));
+});
+
+module.exports = router;
+
