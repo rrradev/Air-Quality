d21dbbc32e8c07c4fdf14911554bf59fc946453e
Radoslav Radev <rrradev@users.noreply.github.com>
2020-06-09 21:57:30 +0300

    Minor fx

--- config/keys.js
+++ config/keys.js
@@ -2,9 +2,7 @@
     var mongoURI;
 
     if(process.env.NODE_ENV ==='production'){
-        mongoURI = 'mongodb+srv://' + process.env.DATABASE_USER + ':' +
-        process.env.DATABASE_PASSWORD + '@homeserver-ayepl.mongodb.net/' + 
-        process.env.DATABASE_NAME + '?retryWrites=true&w=majority';
+        mongoURI = process.env.MONGO_URI;
     } else {
         const secrets = require('../secrets');
         mongoURI = 'mongodb+srv://' + secrets.requestDB(`username`) + ':' +
