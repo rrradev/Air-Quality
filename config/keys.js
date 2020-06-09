900d83532060dda37a91e90a9b372b3f07de4ac1
Rado <radradef@gmail.com>
2020-06-09 21:36:37 +0300

    Minor fx

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,7 @@
     if(process.env.NODE_ENV ==='production'){
         mongoURI = 'mongodb+srv://' + process.env.DATABASE_USER + ':' +
         process.env.DATABASE_PASSWORD + '@homeserver-ayepl.mongodb.net/' + 
-        process.env.DATABASE_NAME + '?retryWrites=true&w=majority';;
+        process.env.DATABASE_NAME + '?retryWrites=true&w=majority';
     } else {
         const secrets = require('../secrets');
         mongoURI = 'mongodb+srv://' + secrets.requestDB(`username`) + ':' +
