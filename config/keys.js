e820257bc8955f5102efc659987e471772543445
Rado <radradef@gmail.com>
2020-06-09 21:12:03 +0300

    Added production related code

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,14 @@
 const secrets = require('../secrets');
 
-module.exports =  {
-    mongoURI: 'mongodb+srv://' + secrets.requestDB(`username`) + ':' +
-    secrets.requestDB('password') + '@homeserver-ayepl.mongodb.net/' + 
-    secrets.requestDB('name') + '?retryWrites=true&w=majority'
-}
+    const mongoURI;
+
+    if(process.env.NODE_ENV ==='production'){
+        mongoURI = 'mongodb+srv://' + process.env.DATABASE_USER + ':' +
+        process.env.DATABASE_PASSWORD + '@homeserver-ayepl.mongodb.net/' + 
+        process.env.DATABASE_NAME + '?retryWrites=true&w=majority';;
+    } else {
+        mongoURI = 'mongodb+srv://' + secrets.requestDB(`username`) + ':' +
+        secrets.requestDB('password') + '@homeserver-ayepl.mongodb.net/' + 
+        secrets.requestDB('name') + '?retryWrites=true&w=majority';
+    }
+module.exports =  mongoURI;
