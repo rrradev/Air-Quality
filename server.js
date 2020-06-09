818e8bb5a50f9931c81d002da8d56f2ccfdd80f4
Radoslav Radev <rrradev@users.noreply.github.com>
2020-06-09 21:12:03 +0300

    Added production related code

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const hourData = require('./routes/api/hour-data');
 const $3hourData = require('./routes/api/3hour-data');
 const $12hourData = require('./routes/api/12hour-data');
 const dayData = require('./routes/api/day-data');
+const path = require('path');
 
 const app = express();
-const port = 3334;
+const port = process.env.PORT || 3334;
 
 app.use(express.json());
 
@@ -26,5 +27,10 @@ app.use('/api/3hour-data', $3hourData);
 app.use('/api/12hour-data', $12hourData);
 app.use('/api/day-data', dayData);
 
-
+if(process.env.NODE_ENV ==='production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
