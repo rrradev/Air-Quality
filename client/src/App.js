6a2704d09b6c5c3c7b05b29f7944efeaa6a53827
Radoslav Radev <rrradev@users.noreply.github.com>
2020-06-23 20:31:35 +0300

    Created ChartArea component

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,13 @@
 import React from 'react';
-import LineChart from './components/LineChart';
-import {Container, Row, Col, Navbar} from 'reactstrap';
 import NavBar from './components/NavBar';
+import ChartArea from './components/ChartArea'
 
 function App() {
-    
+
     return(
         <div>
             <NavBar />
-                <LineChart name="Particulate matter"
-                    datasets={[
-                        {
-                            id: "pm25",
-                            color: "rgb(255,0,255)",
-                            label: "PM25"
-                            
-                        },
-                        {
-                            id: "pm10",
-                            color: "rgba(0, 230, 64, 1)",
-                            label: "PM10"
-                            
-                        }
-                    ]}
-                />
-                <LineChart name="Temperature"
-                    datasets={[
-                        {
-                            id: "temp",
-                            color: "rgba(240, 52, 52, 1)",
-                            label: "Temperature"
-                            
-                        }
-                    ]}
-                />
-                <LineChart name="Humidity"
-                    datasets={[
-                        {
-                            id: "hum",
-                            color: "rgba(30, 139, 195, 1)",
-                            label: "Humidity"
-                        }
-                    ]}
-                />
-            
+            <ChartArea />
         </div>
     );
 }
