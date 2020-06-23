e5c851757bb181ad11fcde2888c9a40e4704d1f3
Radoslav Radev <rrradev@users.noreply.github.com>
2020-06-23 19:44:56 +0300

    Added NavBar

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavLink,
+} from 'reactstrap';
+
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="sm">
+        <NavbarBrand href="/">Charts</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+          </Nav>
+          <NavLink href="https://github.com/radradef">GitHub</NavLink>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
