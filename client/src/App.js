ff9410d1db9d10eb17ce567d1bf66df02810cad8
Rado <radradef@gmail.com>
2020-07-01 19:10:32 +0300

    Code refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import ChartArea from './components/ChartArea'
-import { Card, Container } from 'reactstrap';
+import {Container} from 'reactstrap';
 
 function App() {
 
