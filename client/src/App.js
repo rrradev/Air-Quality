4ae2c5b5ece662eb5fd4d10d0b464afa2971c9bd
Rado <radradef@gmail.com>
2020-07-08 14:27:21 +0300

    Added additional styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-import ChartArea from './components/ChartArea'
+import ChartArea from './components/ChartArea';
+import './App.css';
 
 function App() {
 
     return(
-        <div>
+        <div className="app">
             <NavBar />
             <ChartArea />   
         </div>
