e4701f4cb4eb4cc5c1735fe818faedbe3a930bfd
Radoslav Radev <rrradev@users.noreply.github.com>
2021-07-31 21:56:47 +0300

    Night Mode

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,17 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
-  Nav,
-  NavLink,
+  Nav
 } from 'reactstrap';
 
 const NavBar = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
 
+  const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
+  const [isBulbOn, setIsBulbOn] = useState();
+  const [toggleCount, setCount] = useState(0);
+  const [bulbURL, setBulbURL] = useState("/icons/bulb_off.png");
+
+  const handleBulbClick = () => {
+    setCount(toggleCount + 1);
+
+    if (toggleCount > 30) {
+      return;
+    }
+
+    setIsBulbOn(!isBulbOn);
+    props.isBulbOn(!isBulbOn);
+  }
+
+  useEffect(() => {
+    let localTheme = window.localStorage.getItem('theme');
+    if (localTheme === "dark") {
+      setIsBulbOn(false);
+    } else {
+      setIsBulbOn(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isBulbOn) {
+      setBulbURL("/icons/bulb_on.png");
+    } else {
+      setBulbURL("/icons/bulb_off.png");
+    }
+  }, [isBulbOn]);
 
   return (
     <div>
@@ -21,7 +51,22 @@ const NavBar = (props) => {
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
           </Nav>
-          <NavLink href="https://github.com/radradef/Air-Quality">GitHub</NavLink>
+
+          <img src={bulbURL}
+            onClick={() => handleBulbClick()}
+            alt=""
+            style={
+              {
+                width: "2.1rem",
+                marginRight: "1rem"
+              }
+            }
+          />
+
+          <a href="https://github.com/radradef/Air-Quality">
+            <div>GitHub</div>
+          </a>
+
         </Collapse>
       </Navbar>
     </div>
