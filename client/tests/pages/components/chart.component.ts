05b28abccf95089c30c891a4244f18acd81bce22
Radoslav Radev <radradef@gmail.com>
2025-05-08 18:55:18 +0300

    Refactors chart button group (#60)
    * refactor chart button group
    
    * update tests

--- client/tests/pages/components/chart.component.ts
+++ client/tests/pages/components/chart.component.ts
@@ -3,14 +3,41 @@ import { Locator } from "@playwright/test";
 export default class Chart {
     chartTitle: Locator;
     oneHourButton: Locator;
+    threeHourButton: Locator;
+    twelveHourButton: Locator;
+    dayButton: Locator;
+    weekButton: Locator;
     oneMonthButton: Locator; 
     loadingOverlay: Locator;
 
     constructor($: Locator) {
         this.chartTitle = $.locator('.title-text');
         this.oneHourButton = $.getByRole('button', { name: '1h' });
+        this.threeHourButton = $.getByRole('button', { name: '3h' });
+        this.twelveHourButton = $.getByRole('button', { name: '12h' });
+        this.dayButton = $.getByRole('button', { name: '1d' });
+        this.weekButton = $.getByRole('button', { name: '1w' });
         this.oneMonthButton = $.getByRole('button', { name: '1m' });
         this.loadingOverlay = $.getByTestId('overlay');
+    }
+
+    async clickButton(label = 'hour') {
+        const buttons = {
+            week: this.weekButton,
+            '12 hours': this.twelveHourButton,
+            '3 hours': this.threeHourButton,
+            hour: this.oneHourButton,
+            day: this.dayButton,
+            month: this.oneMonthButton,
+        };
+    
+        const button = buttons[label];
+        
+        if (!button) {
+            throw new Error(`No such button: ${label}`);
+        }
+    
+        await button.click();
     }
 }
 
